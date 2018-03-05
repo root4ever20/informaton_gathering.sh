3ea031125e3a695bc0b554f3c57dda23e55babeb
root4ever20 <36313007+root4ever20@users.noreply.github.com>
2018-03-05 06:47:31 -0500

    Update informaton_gathering.sh

--- informaton_gathering.sh
+++ informaton_gathering.sh
@@ -1,66 +1,114 @@
 #!/bin/bash
-echo "##############################################################"
-echo "#####################                #########################"
-echo "##############  Information_Gatheraining_script ##############" 
-echo "#####################                #########################"
-echo "##############################################################"
-sleep 3
+# Regular Colors
+Black='\e[0;30m'        # Black
+Red='\e[0;31m'          # Red
+Green='\e[0;32m'        # Green
+Yellow='\e[0;33m'       # Yellow
+Blue='\e[0;34m'         # Blue
+Purple='\e[0;35m'       # Purple
+Cyan='\e[0;36m'         # Cyan
+White='\e[0;37m'        # White
+
+# Bold
+BBlack='\e[1;30m'       # Black
+BRed='\e[1;31m'         # Red
+BGreen='\e[1;32m'       # Green
+BYellow='\e[1;33m'      # Yellow
+BBlue='\e[1;34m'        # Blue
+BPurple='\e[1;35m'      # Purple
+BCyan='\e[1;36m'        # Cyan
+BWhite='\e[1;37m'       # White
+
+
+echo -e "$Red##############################################################"
+echo -e "$Red#################                            #################"
+echo -e "$Red#####       Profissional Information Gathering           #####" 
+echo -e "$Red#################                            #################"
+echo -e "$Red##############################################################"
+echo ''
+sleep 3 
+echo -e "\e[1;34mThis Script is Profissional information Gathering"
+echo ''
+sleep 2
+echo -e '\e[1;36m'
 read -p "whats your victim ip adress: " IP
 read -p "whats your target website:" Website
 read -p "whats your target domain name: " Domain 
-echo "#####################     Start_script   #####################"
-echo "#####################     Curl           #####################"
+echo ''
+echo -e "$Red===================================================================="
+echo -e "$Green                            Curl                                  "
+echo -e "$Red===================================================================="
+
+
+echo -e "\e[1;33m"
 curl ipinfo.io/$IP
 sleep 10
-echo''
-echo "#####################     WhatWeb   #########################"
-echo''
+echo ''
+echo -e "$Red===================================================================="
+echo -e "$Green                           WhatWeb                                "
+echo -e "$Red===================================================================="
+echo -e "\e[1;33m"
 whatweb $Domain
 sleep 10
-echo''
-echo "#####################     dnsenum   #########################"
-echo''
+echo ''
+echo -e "$Red===================================================================="
+echo -e "$Green                              Dnsenum                             "
+echo -e "$Red===================================================================="
+echo -e "\e[1;33m"
 dnsenum -p 500 $Domain
+
 sleep 10
-echo''
-echo "#####################      dnsmap    ########################"
-echo''
-dnsmap $Domain 
-sleep 10
-echo''
-echo "#####################  theharvester  ########################"
-echo''
+
+echo ''
+echo -e "$Red===================================================================="
+echo -e "$Green                             Theharvester                         "
+echo -e "$Red===================================================================="
+echo -e '\e[1;33m'
 theharvester -d $Website -l 2000 -b google 
 sleep 10
-echo''
-echo "#####################   wafw00f     #########################"
-echo''
+echo ''
+echo -e "$Red===================================================================="
+echo -e "$Green                             Wafw00f                              "
+echo -e "$Red===================================================================="
+echo -e '\e[1;33m'
 wafw00f $Website -a
 sleep 10
-echo''
-echo "#####################    lbd         ########################"
-echo''
+echo ''
+echo -e "$Red===================================================================="
+echo -e "$Green                             Lbd                                  "
+echo -e "$Red===================================================================="
+echo -e '\e[1;33m'
 lbd $Website
 sleep 10
 echo''
-echo "####################     nmap        ########################"
-echo''
-nmap -F -O -sV  $Website
+echo -e "$Red===================================================================="
+echo -e "$Green                              Nmap                                "
+echo -e "$Red===================================================================="
+echo -e '\e[1;33m'
+nmap -A -O -D RND:90 $IP
 sleep 10
-echo''
-echo "##############################################################"
-echo''
-if [ $? -eq 0 -a 1 ]
-then
-echo''
-echo "##############################################################"
-echo "#####################                #########################"
-echo "#####################  END OF SCRIPT #########################" 
-echo "#####################                #########################"
-echo "##############################################################"
-echo "This Script done by Ahmad Jawabreh"
-echo "Have a nice day :)"
-echo "GoodBye"
-exit
+echo ''
+
+echo -e "$Red===================================================================="
+echo -e "$Green                             SSL scan                             "
+echo -e "$Red===================================================================="
+echo -e '\e[1;33m'
+sslscan $IP
+sleep 2
+echo ''
+echo -e "$Red##############################################################"
+echo ''
 fi
 
+if [ $? -eq 0 -a 1 ]
+then
+echo -e "$Red##############################################################"
+echo -e "$Red#####################                #########################"
+echo -e "$Red#####################  END OF SCRIPT #########################" 
+echo -e "$Red#####################                #########################"
+echo -e "$Red##############################################################"
+echo -e "$Blue This Script done by Ahmad Jawabreh"
+echo -e "$Blue Have a nice day :)"
+echo -e "$Blue GoodBye"
+fi
+exit
